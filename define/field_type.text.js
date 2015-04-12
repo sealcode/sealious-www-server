50bbb3509d78a3a289f085712240b55692e3d9ed
Kuba Orlik <groovy354@gmail.com>
2015-04-12 11:32:59 +0200

    added optional max_length param to text field type

--- define/field_type.text.js
+++ define/field_type.text.js
@@ -1,3 +1,19 @@
+var Promise = require("bluebird");
+
 module.exports = function(field_Type_text){
+
+	field_Type_text.prototype.isProperValue = function(value){
+		return new Promise(function(resolve, reject){
+			if(this.params.max_length===undefined){
+				resolve();
+			}else{
+				if(value.length<this.params.max_length){
+					resolve();
+				}else{
+					reject("Text '" + value + "' has exceeded max length of " + this.params.max_length + " chars.");
+				}
+			}
+		}.bind(this))
+	}
 
 }
