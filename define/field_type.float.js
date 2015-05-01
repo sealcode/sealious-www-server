f20e89fb962f07a80b5916b374d41732c91edb9e
debian-sh <bestmatrix@gmail.com>
2015-05-01 13:24:17 +0200

    added field_type.float - improved 2

--- define/field_type.float.js
+++ define/field_type.float.js
@@ -3,22 +3,19 @@ var Promise = require("bluebird");
 module.exports = function(field_type_float) {
 
     field_type_float.prototype.isProperValue = function(number) {
-        var test = parseFloat(number);
-        var test_2 = isNaN(number);
+        test = parseFloat(number);
 
         return new Promise(function(resolve, reject) {
-            try {
-                if (test === null || test === NaN || test_2 === true) throw e;
-            } catch (e) {
-                reject("Value `" + number + "` is not a float number format.");
-            }
+                if (test === null || test === NaN || isNaN(number) === true){
+                    reject("Value `" + number + "` is not a float number format.");
+                }
             resolve();
         })
     }
 
     field_type_float.prototype.encode = function(number) {
         return new Promise(function(resolve, reject) {
-            resolve(parseFloat(number));
+            resolve(this.test);
         })
     }
 }
