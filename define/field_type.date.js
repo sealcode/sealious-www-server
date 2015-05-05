21352a4dbf7c575848ba39240310591fd3368d61
debian-sh <bestmatrix@gmail.com>
2015-05-05 11:03:21 +0200

    added field type date - first version without params (only calendar date)

--- define/field_type.date.js
+++ define/field_type.date.js
@@ -1,15 +1,16 @@
 var Promise = require("bluebird");
 
-module.exports = function(field_type_float) {
+module.exports = function(field_type_date) {
 
-    field_type_float.prototype.isProperValue = function(date) {
+
+    field_type_date.prototype.isProperValue = function(date) {
         //YYYY-MM-DD
         return new Promise(function(resolve, reject) {
 
             var date_in_string = date.toString();
 
             var regex_per_day = /^[1-2][0-9]{3}-([1-9]|0[1-9]|1[0-2])-([1-9]|0[1-9]|1[0-9]|2[0-9]|30|31)$/; //granulation_per_day
-			var regex_per_time = //granulation_per_time
+			//var regex_per_time = //granulation_per_time
  
 
             if (regex.test(date_in_string) != true ||) {
@@ -20,7 +21,7 @@ module.exports = function(field_type_float) {
         })
     }
 
-    field_type_float.prototype.encode = function(date) {
+    field_type_date.prototype.encode = function(date) {
         return new Promise(function(resolve, reject) {
             resolve(Date.parse(date));
         })
