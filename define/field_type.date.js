4609009538a7082d304488c6b4b45ca155a5a8c9
debian-sh <bestmatrix@gmail.com>
2015-05-05 21:46:10 +0200

    added field_type.date and field_type.datetime - improved

--- define/field_type.date.js
+++ define/field_type.date.js
@@ -9,17 +9,17 @@ module.exports = function(field_type_date) {
 
             var regex = /^([0-9]{4})-([1-9]|0[1-9]|1[0-2])-([1-9]|0[1-9]|1[0-9]|2[0-9]|30|31)$/; //granulation_per_day
 
-	            if (regex.test(date_in_string) === false || Date.parse(date_in_string) === NaN) {
-	                reject("`"+date+"`" + " is not date calendar format. Expected value standard IS0 8601 (YYYY-MM-DD)");
-	            } else {
-	                resolve();
-	            }
+                if (regex.test(date_in_string) === false || Date.parse(date_in_string) === NaN) {
+                    reject("Value `"+date+"`" + " is not date calendar format. Expected value standard IS0 8601 (YYYY-MM-DD)");
+                } else {
+                    resolve();
+                }
         })
     }
 
     field_type_date.prototype.encode = function(date) {
         return new Promise(function(resolve, reject) {
-        	var date_in_string = date.toString();
+            var date_in_string = date.toString();
             resolve(Date.parse(date_in_string));
         })
     }
--- define/field_type.datetime.js
+++ define/field_type.datetime.js
@@ -4,15 +4,12 @@ module.exports = function(field_type_datetime) {
 
     field_type_datetime.prototype.isProperValue = function(datetime) {
         return new Promise(function(resolve, reject) {
-            var parsed_datetime = parseInt(datetime);
 
-            var regex = /^(-[1-9]{1,11})$|^([0-9]|[1-9][0-9]*)$/; //granulation_per_timestamp
-
-                if (regex.test(parsed_datetime) === false) {
-                    reject("`"+datetime+"`" + " is not datetime format. Expected value is timestamp.");
-                } else {
-                    resolve();
-                }
+            if (isNaN(datetime) === true || parseInt(datetime) === NaN) {
+                reject("Value `" + datetime + "`" + " is not datetime format. Expected value is timestamp.");
+            } else {
+                resolve();
+            }
         })
     }
 
