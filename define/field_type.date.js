e6684214d2eb676fd2444eb556d5e35cbedb1b59
debian-sh <bestmatrix@gmail.com>
2015-05-05 11:40:38 +0200

    added field_type.date

--- define/field_type.date.js
+++ define/field_type.date.js
@@ -10,7 +10,7 @@ module.exports = function(field_type_date) {
             var regex = /^[1-2][0-9]{3}-([1-9]|0[1-9]|1[0-2])-([1-9]|0[1-9]|1[0-9]|2[0-9]|30|31)$/; //granulation_per_day
 
 	            if (regex.test(date_in_string) === false || Date.parse(date_in_string) === NaN) {
-	                reject(date + " is not date calendar format. Expected value standard IS0 8601 (YYYY-MM-DD) for date without time. If you would like to use `date with time` please set field_type_datetime instead field_type_date in main.js");
+	                reject("`"+date+"`" + " is not date calendar format. Expected value standard IS0 8601 (YYYY-MM-DD) for date without time. If you would like to use `date with time` please set field_type_datetime instead field_type_date in main.js");
 	            } else {
 	                resolve();
 	            }
