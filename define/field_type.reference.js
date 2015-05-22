3639e60037bcbb162e2ab5dfb024193e37743500
Kuba Orlik <groovy354@gmail.com>
2015-05-22 11:04:33 +0200

    checkpoint

--- define/field_type.reference.js
+++ define/field_type.reference.js
@@ -1,5 +1,4 @@
 var Promise = require("bluebird");
-
 /*
 var Reference = function(declaration){
 	this.validate_declaration(declaration);
@@ -29,64 +28,67 @@ module.exports = function(field_type_reference){
 		}
 	}
 
-	field_type_reference.prototype.isProperValue = function(value){
-		return new Promise(function(resolve, reject){
-			if(typeof value == "object"){
-				//validate object's values as values for new resource
-				var type = value.type;
-				if(type===undefined){
-					reject("Reference resource type undefined. `type` attribute should be set to one of these values: " + this.params.allowed_types.join(", ") + ".");
-				}else if(this.params.allowed_types.indexOf(type)==-1){
-					reject("Incorrect reference resource type: `" +  type + "`. Allowed resource types for this reference are:" + this.params.allowed_types.join(", ") + "."); 
-				}else{
-					var resource_type_object = Sealious.ChipManager.get_chip("resource_type", type);
-					
-					resource_type_object.validate_field_values(value.data)
-						.then(resolve)
-						.catch(function(error){
-							if(error.is_sealious_error && error.data.invalid_fields==undefined){
-								reject(error.status_message);
-							}else{
-								reject(error.data.invalid_fields);							
-							}
-						});
-				}
+	field_type_reference.prototype.isProperValue = function(context, value){
+		console.log("reference.isProperValue:", arguments);
+		if(typeof value == "object"){
+			//validate object's values as values for new resource
+			var type = value.type;
+			if(type===undefined){
+				return Promise.reject("Reference resource type undefined. `type` attribute should be set to one of these values: " + this.params.allowed_types.join(", ") + ".");
+			}else if(this.params.allowed_types.indexOf(type)==-1){
+				return Promise.reject("Incorrect reference resource type: `" +  type + "`. Allowed resource types for this reference are:" + this.params.allowed_types.join(", ") + "."); 
 			}else{
-				//value is uid. Check if it is proper
-				var supposed_resource_id = value;
-				Sealious.Dispatcher.resources.get_by_id(supposed_resource_id)
-				.then(function(resource){
-					if(this.params.allowed_types.indexOf(resource.type)>=0){
-						resolve(resource);
-					}else{
-						reject("Resource of id `" + supposed_resource_id + "` is not of allowed type. Allowed types are: [" + this.params.allowed_types.join(", ") +"]");
-					}
-				}.bind(this))
+				var resource_type_object = Sealious.ChipManager.get_chip("resource_type", type);
+				var access_strategy = resource_type_object.get_access_strategy();
+				
+				return access_strategy.check(context, value.type, value.data)
+				.then(function(){
+					return resource_type_object.validate_field_values(context, value.data);					
+				})
 				.catch(function(error){
-					if(error.type=="not_found"){
-						reject(error.status_message);
+					if(error.is_sealious_error && error.data.invalid_fields==undefined){
+						return Promise.reject(error.status_message);
 					}else{
-						reject(error);
+						return Promise.reject(error.data.invalid_fields);							
 					}
-				}.bind(this));
+				});
 			}
-		}.bind(this));
+		}else{
+			//value is uid. Check if it is proper
+			var supposed_resource_id = value;
+			console.log(supposed_resource_id);
+			return Sealious.Dispatcher.resources.get_by_id(context, supposed_resource_id)
+			.then(function(resource){
+				if(this.params.allowed_types.indexOf(resource.type)>=0){
+					return Promise.resolve(resource);
+				}else{
+					return Promise.reject("Resource of id `" + supposed_resource_id + "` is not of allowed type. Allowed types are: [" + this.params.allowed_types.join(", ") +"]");
+				}
+			}.bind(this))
+			.catch(function(error){
+				if(error.type=="not_found"){
+					return Promise.reject(error.status_message);
+				}else{
+					return Promise.reject(error);
+				}
+			}.bind(this));
+		}
 	}
+
+	field_type_reference.prototype.isProperValue.uses_context = true;
 
 	field_type_reference.prototype.isProperValue.has_byproducts = true;
 
 	field_type_reference.prototype.encode = function(context, value_in_code){
 		//decide whether to create a new resource (if so, do create it). Resolve with id of referenced resource.
-		return new Promise(function(resolve, reject){
-			if(value_in_code instanceof Object){
-				Sealious.Dispatcher.resources.create(context, value_in_code.type, value_in_code.data).then(function(resource){
-					resolve(resource.id);
-				})
-			}else{
-				//assuming the provided id already exists
-				resolve(value_in_code);
-			}			
-		})
+		if(value_in_code instanceof Object){
+			return Sealious.Dispatcher.resources.create(context, value_in_code.type, value_in_code.data).then(function(resource){
+				return Promise.resolve(resource.id);
+			})
+		}else{
+			//assuming the provided id already exists
+			return Promise.resolve(value_in_code);
+		}			
 	}
 
 	field_type_reference.prototype.encode.uses_context = true;
