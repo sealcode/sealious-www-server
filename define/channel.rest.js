95161dd679e318458a982bbfe1170ef7ec72fc90
Kuba Orlik <groovy354@gmail.com>
2015-05-26 19:47:13 +0200

    fixed indentation

--- define/channel.rest.js
+++ define/channel.rest.js
@@ -2,7 +2,7 @@ module.exports = function(channel, dispatcher, dependencies) {
 
     var www_server = dependencies["channel.www_server"];
 
-	var get_context = www_server.get_context;
+    var get_context = www_server.get_context;
 
     channel.set_url_base = function(base_url) {
         var resource_types = Sealious.ChipManager.get_all_resource_types();
@@ -13,38 +13,38 @@ module.exports = function(channel, dispatcher, dependencies) {
 
     }
 
-        channel.add_path = function(url, resource_type_name){
+    channel.add_path = function(url, resource_type_name){
 
-            var resource_type_object = Sealious.ChipManager.get_chip("resource_type", resource_type_name);
+        var resource_type_object = Sealious.ChipManager.get_chip("resource_type", resource_type_name);
 
-            www_server.route({
-                method: "GET",
-                path: url+"/signature",
-                handler: function(request, reply, context){
-                    dispatcher.resources.get_resource_type_signature(context, resource_type_name)
-                    .then(reply, reply);
-                }
+        www_server.route({
+            method: "GET",
+            path: url+"/signature",
+            handler: function(request, reply, context){
+                dispatcher.resources.get_resource_type_signature(context, resource_type_name)
+                .then(reply, reply);
+            }
                 // hanlder GET ma wypisać wszystkie zasoby o podanym typie
             });
 
-            www_server.route({
-                method: "GET",
-                path: url,
-                handler: function(request, reply){
-                    var context = get_context(request);
-                    dispatcher.resources.list_by_type(context, resource_type_name)
-                    .then(reply, reply);
-                }
-            });
+        www_server.route({
+            method: "GET",
+            path: url,
+            handler: function(request, reply){
+                var context = get_context(request);
+                dispatcher.resources.list_by_type(context, resource_type_name)
+                .then(reply, reply);
+            }
+        });
 
 
-            www_server.route({
-                method: "POST",
-                path: url,
-                config: {
-                    payload: {
-                        maxBytes: 209715200,
-                        output: resource_type_object.has_large_data_fields()? 'stream' : "data",
+        www_server.route({
+            method: "POST",
+            path: url,
+            config: {
+                payload: {
+                    maxBytes: 209715200,
+                    output: resource_type_object.has_large_data_fields()? 'stream' : "data",
                         //parse: true
                     },
                     handler: function(request, reply){
@@ -58,39 +58,39 @@ module.exports = function(channel, dispatcher, dependencies) {
                 // handler POST ma stworzyć zasób z podanymi wartościami
             });
 
-            www_server.route({
-                method: "DELETE",
-                path: url+"/{id}",
-                handler: function(request, reply){
-                    var context = get_context(request);
-                    dispatcher.resources.delete(context, resource_type_name, request.params.id).then(function(response){
-                        reply("", 204);
-                    }, reply);
-                }
-            });
+        www_server.route({
+            method: "DELETE",
+            path: url+"/{id}",
+            handler: function(request, reply){
+                var context = get_context(request);
+                dispatcher.resources.delete(context, resource_type_name, request.params.id).then(function(response){
+                    reply("", 204);
+                }, reply);
+            }
+        });
 
-            www_server.route({
-                method: "GET",
-                path: url+"/{id}",
-                handler: function(request, reply){
-                    var context = get_context(request);
-                    dispatcher.resources.get_by_id(context, request.params.id).then(function(response){
-                        reply(response);
-                    }).catch(function(error){
-                        reply(error);
-                    });
-                }
-            });
+        www_server.route({
+            method: "GET",
+            path: url+"/{id}",
+            handler: function(request, reply){
+                var context = get_context(request);
+                dispatcher.resources.get_by_id(context, request.params.id).then(function(response){
+                    reply(response);
+                }).catch(function(error){
+                    reply(error);
+                });
+            }
+        });
 
-            www_server.route({
-                method: "PUT",
-                path: url+"/{id}",
-                handler: function(request, reply){
-                    var context = get_context(request);
-                    dispatcher.resources.update_resource(context, request.params.id, request.payload).then(function(response){
-                        reply(response);
-                    });
-                }
-            });     
-        }
+        www_server.route({
+            method: "PUT",
+            path: url+"/{id}",
+            handler: function(request, reply){
+                var context = get_context(request);
+                dispatcher.resources.update_resource(context, request.params.id, request.payload).then(function(response){
+                    reply(response);
+                });
+            }
+        });     
+    }
 }
