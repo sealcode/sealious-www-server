5d99300367eb4fc8edec5a614bfb49f544039749
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-03 13:14:45 +0100

    Add support for 201 http response code

--- handle-request.js
+++ handle-request.js
@@ -18,9 +18,12 @@ function handle_request(app, request, reply){
 			return action.run(context, body);
 		})
 		.then(function(response){
+			console.log(response);
 			if(response instanceof app.Sealious.Responses.NewSession){
 				reply(response).state(config["session-cookie-name"], response.metadata.session_id);
-			}else{
+			}else if(response instanceof app.Sealious.Responses.ResourceCreated){
+				reply(response).code(201);
+			} else {
 				reply(response);
 			}
 		})
