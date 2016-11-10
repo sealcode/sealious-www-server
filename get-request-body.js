cc8e047f958daf04e0d557d4b96ffabb0c722e6a
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-10 14:56:52 +0100

    Square-parse the request payload, as well

--- get-request-body.js
+++ get-request-body.js
@@ -8,16 +8,18 @@ function get_request_body(context, request){
 	for(const i in request.query){
 		squares.set(parsed_query, i, request.query[i]);
 	}
-	const body = merge(request.payload, parsed_query);
+	for(const i in request.payload){
+		squares.set(parsed_query, i, request.payload[i]);
+	}
 	for(var i in request.payload){
 		if(request.payload[i].payload && request.payload[i].payload instanceof Buffer){
 			let filename = request.payload[i].filename;
 			var mime_type = request.payload[i].headers["content-type"];
 			var data = request.payload[i].payload;
-			body[i] = new Sealious.File(context, filename, data, null, mime_type);
+			parsed_query[i] = new Sealious.File(context, filename, data, null, mime_type);
 		}
 	}
-	return body;
+	return parsed_query;
 };
 
 module.exports = get_request_body;
