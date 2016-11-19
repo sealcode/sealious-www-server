4669e446843202b3fdf4df34d08b5ec931e8dfe9
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-11-19 21:33:53 +0100

    Fix error in param parsing code

--- get-request-body.js
+++ get-request-body.js
@@ -4,16 +4,29 @@ const Sealious = require("sealious");
 
 const squares = {
 	set: function(obj, key, value){
-		const keys = key.split(/[\]\[]{1,2}/g).filter(s=>s!="");
-		let current = obj;
-		const last_key = keys.splice(-1)[0];
-		for(const i in keys){
-			if(current[keys[i]] === undefined){
-				current[keys[i]] = {};
-			}
-			current = current[keys[i]];
+		const keys = key.split(/[\]\[]{1,2}/g);
+		if(keys.length > 1){
+			keys.splice(-1); //to remove the trailing empty string;
 		}
-		current[last_key] = value;
+		const last_key = keys[keys.length-1];
+		let current = obj;
+		for(let i=0; i<keys.length-1; i++){
+			const key = keys[i];
+			const next_key = keys[i+1];
+			if(current[key] === undefined){
+				if(next_key === ""){
+					current[key] = [];
+				}else{
+					current[key] = {};
+				}
+			}
+			current = current[key];
+		}
+		if(last_key === ""){
+			current.push(value);
+		}else{
+			current[last_key] = value;
+		}
 	}
 };
 
@@ -33,7 +46,10 @@ function get_request_body(context, request){
 			parsed_query[i] = new Sealious.File(context, filename, data, null, mime_type);
 		}
 	}
+	console.log("parsed query:", JSON.stringify(parsed_query));
 	return parsed_query;
 };
+
+const a = {};
 
 module.exports = get_request_body;
